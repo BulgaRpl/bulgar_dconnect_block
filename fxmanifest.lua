a122355636e8a6baf433cd5f15fc418b18fa8eb2
BulgaRpl <69915160+BulgaRpl@users.noreply.github.com>
2022-06-19 00:56:18 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "adamant"
+games {"rdr3","gta5"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+author 'BulgaR' -- https://discord.gg/dcyY2e5kb3
+
+lua54 'yes'
+
+server_script {
+    'server.lua'
+}
