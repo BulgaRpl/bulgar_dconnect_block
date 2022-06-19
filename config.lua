034b9db8518ed10fe9157eaf5a5002550f9d27ec
BulgaRpl <69915160+BulgaRpl@users.noreply.github.com>
2022-06-19 15:02:16 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,4 @@
+Config = {}
+
+-- Set this to 'steam' if u want to use Steam verification / Set this to 'license' if u want to use Rockstar License verification.
+Config.VerificationMethod = 'steam' -- or 'license'
