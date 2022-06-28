d697b7c9009075fed9e7445656b49ad9536113e6
BulgaRpl <69915160+BulgaRpl@users.noreply.github.com>
2022-06-28 19:42:02 +0200

    Add Double Verification Method and Discord Logs

--- config.lua
+++ config.lua
@@ -2,3 +2,11 @@ Config = {}
 
 -- Set this to 'steam' if u want to use Steam verification / Set this to 'license' if u want to use Rockstar License verification.
 Config.VerificationMethod = 'steam' -- or 'license'
+Config.VerifyBoth = false -- Set to true if u want to verify Steam and Rockstar License "Config.VerificationMethod" will be ignored in this case.
+
+-- Discord Logs
+Config.DiscordWebhookLink = 'https://discord.com/api/webhooks/'
+Config.DiscordColor = '6749952'
+Config.DiscordUserName = 'BulgaR Dconnect Blocker'
+Config.DiscordTitle = 'Attempting to Double Enter the Server'
+Config.EnableDiscordLogs = false
