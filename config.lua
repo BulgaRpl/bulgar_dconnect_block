daea955e66b0f1cc41a11b6d3d91ade2aec60be0
BulgaRpl <69915160+BulgaRpl@users.noreply.github.com>
2022-08-29 18:31:30 +0200

    VerifyBoth is now set to "true" by default

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 -- Set this to 'steam' if u want to use Steam verification / Set this to 'license' if u want to use Rockstar License verification.
 Config.VerificationMethod = 'steam' -- or 'license'
-Config.VerifyBoth = false -- Set to true if u want to verify Steam and Rockstar License "Config.VerificationMethod" will be ignored in this case.
+Config.VerifyBoth = true -- Set to true if u want to verify Steam and Rockstar License "Config.VerificationMethod" will be ignored in this case.
 
 -- Discord Logs
 Config.DiscordWebhookLink = 'https://discord.com/api/webhooks/'
